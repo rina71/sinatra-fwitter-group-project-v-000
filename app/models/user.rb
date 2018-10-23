f00c2d3bf3c3c2a01e75b1131d353d2ab9c19133
Yurina Shin <yurinas@gmail.com>
2018-10-23 21:22:32 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
   def self.find_by_slug(slug)
 
-    User.all.each{|user| binding.pry
-      user.username.slug == slug}
+    User.all.each{|user| user.slug == slug}
   end
 
   def slug
