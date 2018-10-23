3f581ef78bf2a626812a7ca7e3b6a9296171fbb3
Yurina Shin <yurinas@gmail.com>
2018-10-23 21:19:37 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
   def self.find_by_slug(slug)
-binding.pry
+
     User.all.each{|user| user.username.slug == slug}
   end
 
   def slug
+    binding.pry
     self.username.downcase.gsub(' ','-')
   end
 end
