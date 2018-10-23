28e1ddf0cb82de790f437346bf31217218f3e572
Yurina Shin <yurinas@gmail.com>
2018-10-23 21:29:34 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
   def self.find_by_slug(slug)
-    User.all.finddo |user|
-      binding.pry
+    User.all.find do |user|
+
       user.slug == slug
     end
   end
