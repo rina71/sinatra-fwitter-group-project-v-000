64cd69efd0b68fe14f1cfd1cb9d2d11969542152
Yurina Shin <yurinas@gmail.com>
2018-10-23 20:42:31 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -47,7 +47,7 @@ class TweetsController < ApplicationController
     end
   end
 
-  post "/tweets/:id/edit" do
+  patch "/tweets/:id/edit" do
 
     tweet = Tweet.find_by_id(params[:id])
     if params[:content] == ""
